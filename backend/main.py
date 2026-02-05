77c63e623539c4cb730a0a0e471696d8d09a21e7
shahedkhanin-dev <shahed.khan.in@gmail.com>
2026-02-05 12:09:25 +0530

    ATS scoring engine implemented

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Form
+from PyPDF2 import PdfReader
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "AI Resume Analyzer Backend Running"}
+def extract_text_from_pdf(file):
+    reader = PdfReader(file)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
+
+@app.post("/analyze")
+async def analyze_resume(
+        resume: UploadFile = File(...),
+        job_description: str = Form(...)
+):
+    resume_text = extract_text_from_pdf(resume.file)
+
+    text = [resume_text, job_description]
+
+    cv = CountVectorizer().fit_transform(text)
+    similarity_score = cosine_similarity(cv)[0][1]
+
+    ats_score = round(similarity_score * 100, 2)
+
+    return {
+        "ATS_score": ats_score
+    }
